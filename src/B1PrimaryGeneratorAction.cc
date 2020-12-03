6138a610d10ba9ab8c49f6ff4c73d9444e2c7d3f
WOOYOUNG JANG <wyjang.physics@gmail.com>
2020-12-03 15:36:18 -0600

    resolved warning related to unused variables - the variables 'G4double envSizeXY' and 'G4double envSizeZ' are deleted together with the relavant code blocks

--- src/B1PrimaryGeneratorAction.cc
+++ src/B1PrimaryGeneratorAction.cc
@@ -77,35 +77,6 @@ void B1PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
   // on DetectorConstruction class we get Envelope volume
   // from G4LogicalVolumeStore.
 
-  G4double envSizeXY = 0;
-  G4double envSizeZ = 0;
-
-  if (!fEnvelopeBox)
-  {
-    G4LogicalVolume* envLV
-      = G4LogicalVolumeStore::GetInstance()->GetVolume("Envelope");
-    if ( envLV ) fEnvelopeBox = dynamic_cast<G4Box*>(envLV->GetSolid());
-  }
-
-  if ( fEnvelopeBox ) {
-    envSizeXY = fEnvelopeBox->GetXHalfLength()*2.;
-    envSizeZ = fEnvelopeBox->GetZHalfLength()*2.;
-  }
-  else  {
-    G4ExceptionDescription msg;
-    msg << "Envelope volume of box shape not found.\n";
-    msg << "Perhaps you have changed geometry.\n";
-    msg << "The gun will be place at the center.";
-    G4Exception("B1PrimaryGeneratorAction::GeneratePrimaries()",
-     "MyCode0002",JustWarning,msg);
-  }
-
-  /*
-  G4double size = 0.8;
-  G4double x0 = size * envSizeXY * (G4UniformRand()-0.5);
-  G4double y0 = size * envSizeXY * (G4UniformRand()-0.5);
-  G4double z0 = -0.5 * envSizeZ;
-  */
   G4double x0 = 0.;
   G4double y0 = 0.;
   G4double z0 = -8000.;
